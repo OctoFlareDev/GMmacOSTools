2c0950747681fab9c974337f9dc680502919ce68
OctoFlare <62318879+OctoFlareDev@users.noreply.github.com>
2025-08-04 15:59:55 +0800

    Initial Commit

---
+++ GMmacOSTools/GMmacOSToolsPriv.hpp
@@ -0,0 +1,17 @@
+//
+//  GMmacOSToolsPriv.hpp
+//  GMmacOSTools
+//
+//  Created by Flare ​ on 8/4/25.
+//
+
+/* The classes below are not exported */
+#pragma GCC visibility push(hidden)
+
+class GMmacOSToolsPriv
+{
+    public:
+    void HelloWorldPriv(const char *);
+};
+
+#pragma GCC visibility pop
